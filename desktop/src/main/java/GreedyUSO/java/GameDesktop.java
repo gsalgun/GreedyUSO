89fed59f491204d911048ee9213b22e93a50ccfe
gsalgun <guven@peakgames.net>
2014-01-20 10:02:40 +0200

    Initial commit

---
+++ desktop/src/main/java/GreedyUSO/java/GameDesktop.java
@@ -0,0 +1,14 @@
+package GreedyUSO.java;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import GreedyUSO.core.Game;
+
+public class GameDesktop {
+	public static void main (String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.useGL20 = true;
+		new LwjglApplication(new Game(), config);
+	}
+}
