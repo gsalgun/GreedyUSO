55e7e82ddd95b30ccef919bbbaa43b7248f9f1fb
gsalgun <guven@peakgames.net>
2014-01-20 11:53:11 +0200

    Cilgin tirtil

--- desktop/src/main/java/GreedyUSO/java/GameDesktop.java
+++ desktop/src/main/java/GreedyUSO/java/GameDesktop.java
@@ -4,11 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import GreedyUSO.core.Game;
+import com.badlogic.gdx.utils.GdxNativesLoader;
 
 public class GameDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
+//        config.b
+        GdxNativesLoader.load();
 		new LwjglApplication(new Game(), config);
 	}
 }
